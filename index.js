cbf33faca864f5c299d9389764559fdd0de15028
PeiDa Kuo <guo.peida@gmail.com>
2018-10-17 22:22:49 -0500

    refactor data requests to getReq; show different bike paths' widths

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 import * as L from "leaflet";
 import "leaflet-providers";
 
-// console.log("new index.js");
 
+function getReq(url, successHandler) {
+  var req = new XMLHttpRequest();
+  req.open("GET", url);
+
+  req.onreadystatechange = function () {
+    if (req.readyState === 4 && req.status === 200) {
+      successHandler(req.responseText);
+    };
+  };
+  req.send();
+}
 
 document.onreadystatechange = function() {
   if (document.readyState === "complete") {
@@ -21,51 +31,56 @@ document.onreadystatechange = function() {
     L.tileLayer.provider('Stamen.Watercolor').addTo(madison);
 
     var overview = function () {
-      var req = new XMLHttpRequest();
-      req.open("GET", "https://opendata.arcgis.com/datasets/66e4a6a80ae64865a81bc8d4464a6417_12.geojson");
-      req.onreadystatechange = function () {
-        if (req.readyState === 4 && req.status === 200) {
-          var neighborhoods = JSON.parse(req.responseText);
-          L.geoJSON(neighborhoods, {
-            style: {
-              color: "green",
-              weight: 2,
-              fillColor: "gray"
-            },
-            onEachFeature: function (feature, layer) {
-              layer.bindTooltip(feature.properties.NEIGHB_NAME.replace(/ (Neighborhood|Community|Homeowners|Condominium|Village|Property Owners) Association$/, ""),
-              {
-                className: "tip",
-                opacity: 0.8,
-                direction: "center"
-              });
-            }
-          }).addTo(madison);
-        }
-      };
+      var neighborhoodsUrl = "https://opendata.arcgis.com/datasets/66e4a6a80ae64865a81bc8d4464a6417_12.geojson";
 
-      req.send();
+      function drawOverviewMap(respText) {
+        var neighborhoods = JSON.parse(respText);
+
+        L.geoJSON(neighborhoods, {
+          style: {
+            color: "green",
+            weight: 2,
+            fillColor: "gray"
+          },
+          onEachFeature: function (feature, layer) {
+            layer.bindTooltip(feature.properties.NEIGHB_NAME.replace(/ (Neighborhood|Community|Homeowners|Condominium|Village|Property Owners) Association$/, ""),
+            {
+              className: "tip",
+              opacity: 0.8,
+              direction: "center"
+            });
+          }
+        }).addTo(madison);
+      }
+
+      getReq(neighborhoodsUrl, drawOverviewMap);
+
     };
 
     var bikers =  function () {
-      var req = new XMLHttpRequest();
-      req.open("GET", "https://opendata.arcgis.com/datasets/eea6d3fb4ca64f8199d9e1482ff45ae2_11.geojson");
-      req.onreadystatechange = function () {
-        if (req.readyState === 4 && req.status === 200) {
-          var neighborhoods = JSON.parse(req.responseText);
-          L.geoJSON(neighborhoods, {
-            style: {
-              color: "blue",
-              weight: 2,
-              fillColor: "gray"
-            }
-          }).addTo(madison);
-          console.log(JSON.parse(req.responseText));
-        }
-      };
+      var bikePathsUrl = "https://opendata.arcgis.com/datasets/eea6d3fb4ca64f8199d9e1482ff45ae2_11.geojson";
 
-      req.send();
+      function drawBikePaths(respText) {
+        var bikePaths = JSON.parse(respText);
+        L.geoJSON(bikePaths, {
+          style: function (feature, layer){
+            // Show bike paths' widths, with default of 8 for null values
+            return {
+              color: "blue",
+              weight: (feature.properties.BikePaWdth||8)/2,
+              fillColor: "gray"
+            };
+          },
+          onEachFeature: function (feature, layer) {
+            layer.bindTooltip(String(feature.properties.BikePaWdth)).addTo(madison);
+          }
+        }).addTo(madison);
+      }
+
+      getReq(bikePathsUrl, drawBikePaths);
     };
+
+    // Default map
     bikers();
   }
 
